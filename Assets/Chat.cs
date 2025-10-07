803f75787eca7bc9c203e5c84b639044d610dfed
Cameron Bonde <c.bonde@sae.edu>
2025-10-07 16:16:46 +1100

    UI update. Client to server

--- Assets/Chat.cs
+++ Assets/Chat.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 public class Chat : NetworkBehaviour
 {
 	public TextMeshProUGUI messageText;
-		
+
 	// Server to Clients
 	[Rpc(SendTo.ClientsAndHost, RequireOwnership = true, Delivery = RpcDelivery.Reliable)]
-	public void SendMessageToClients_Rpc(string message)
+	public void SendMessageToClientsFromServer_Rpc(string message)
 	{
 		Debug.Log("Server: " + message);
-		messageText.text += "Server: "+message + "\n";
+		messageText.text += "Server: " + message + "\n";
 	}
 
+	// Client to Server
 	[Rpc(SendTo.Server, RequireOwnership = true, Delivery = RpcDelivery.Reliable)]
 	public void SendMessageToServerFromClient_Rpc(string message)
 	{
-		Debug.Log("Client: "+message);
-		messageText.text += "Client: "+message + "\n";
-	}
-	
-	// Client to Server
-	[Rpc(SendTo.Server, RequireOwnership = false, Delivery = RpcDelivery.Reliable)]
-	public void SendMessageToServerFromClient(string message)
-	{
-		Debug.Log("*** Message from client: "+message);
+		Debug.Log("*** Message from client: " + message);
 	}
 }
